c79f4812a6570fa11612dde5b0ac220e66e074ad
Abednego Wambua <abednego.k.wambua@gmail.com>
2019-03-08 05:11:29 +0300

    Added vue js and admin lte template

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('{path}', 'HomeController@index')->name('path','([A-z\d-\/_.]+)?');

de7a2c900edbb29d5d495a6eda094dca002c7b02
Abednego Wambua <abednego.k.wambua@gmail.com>
2019-03-12 07:39:52 +0300

    added payments and project

---
+++ resources/js/components/Project/ProjectDetail.vue
@@ -0,0 +1,158 @@
+<template>
+    <div class="container">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>150</h3>
+
+                            <p>Members</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                            <p>Members Contribution</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-stats-bars"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>44</h3>
+
+                            <p>Projects Running</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-person-add"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>65</h3>
+
+                            <p>Projects Completed</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-pie-graph"></i>
+                        </div>
+                        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <!-- ./col -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="card">
+                        <div class="card-header border-transparent">
+                            <h3 class="card-title">Latest Payments</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-widget="collapse">
+                                    <i class="fa fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-widget="remove">
+                                    <i class="fa fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body p-0">
+                            <div class="table-responsive">
+                                <table class="table m-0">
+                                    <thead>
+                                    <tr>
+                                        <th>Pament ID</th>
+                                        <th>Amount</th>
+                                        <th>User</th>
+                                        <th>Project</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr v-for="payments in all_payments">
+                                        <td><a href="pages/examples/invoice.html">{{payments.payment_reference}}</a>
+                                        </td>
+                                        <td>{{payments.payment_amount}}</td>
+                                        <td><span class="badge ">{{payments.user.name}}</span></td>
+                                        <td>
+                                           {{payments.project.project_name}}
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.table-responsive -->
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer clearfix">
+                            <a href="javascript:void(0)" class="btn btn-sm btn-info float-left">Place New Paymnet</a>
+                            <a href="javascript:void(0)" class="btn btn-sm btn-secondary float-right">View All
+                                Payments</a>
+                        </div>
+                        <!-- /.card-footer -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props: ['chamaa_id'],
+        data() {
+            return {
+                all_payments: [],
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }, methods: {
+            fetch_payments() {
+                this.$store.dispatch('get_payments', this.chamaa_id)
+            }
+        },
+        computed: {
+            load_all_payments() {
+                return this.all_payments = this.$store.getters.ALL_PAYMENTS
+            }
+        },
+        created() {
+            this.fetch_payments();
+        },
+        watch: {
+            load_all_payments(new_, old) {
+                if (new_ != old) {
+                    this.all_payments = new_;
+                }
+            }
+
+        }
+    }
+</script>
---
+++ resources/js/components/Project/Projects.vue
@@ -0,0 +1,160 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card card-default">
+                    <div class="card-header">Chamaa
+
+                        <button class="btn btn-success right"
+                                @click="open_my_modal">
+                            New Chamaa
+                            <i class="nav-icon fas fa-plus-square "></i>
+                        </button>
+                    </div>
+
+                    <div class="card-body">
+                        <div class="row">
+                            <div v-for="chama in all_chamas" class="col-md-3 col-sm-6 col-12">
+                                <div class="info-box bg-info">
+                                    <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
+
+                                    <div class="info-box-content">
+                                        <span class="info-box-text">{{chama.chamaa_name}}</span>
+                                        <span class="info-box-number">{{chama.members_count}} Members</span>
+
+                                        <div class="progress">
+                                            <div class="progress-bar" :style="{width: chama.members_count+'%'}"></div>
+                                        </div>
+                                        <span class="progress-description">
+                                          30% Increase in 30 Days
+                                        </span>
+                                        <a class="btn" @click="view_chamaa(chama.id)">View<i
+                                            class=" fas fa-tachometer-alt"></i></a>
+                                    </div>
+                                    <!-- /.info-box-content -->
+                                </div>
+                                <!-- /.info-box -->
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade in" id="create_chamaa" role="dialog">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+
+
+                    <div class="modal-body">
+                        <!-- form start -->
+                        <h5 v-show="!editMode" class="modal-title">New Chamaa</h5>
+                        <h5 v-show="editMode" class="modal-title">Update Chamaa's Information</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form @submit.prevent="editMode ? update_chamaa():create_chamaa()">
+                        <div class="modal-body">
+                            <div class="form-group">
+
+                                <div class="row">
+                                    <div class="col-3">
+                                        <label for="chamaa_name"></label>
+                                    </div>
+                                </div>
+                                <input v-model="form.chamaa_name" type="text" name="chamaa_name"
+                                       placeholder="Chamaa Name"
+                                       id="chamaa_name"
+                                       class="form-control" :class="{ 'is-invalid': form.errors.has('project_ref') }">
+                                <has-error :form="form" field="project_ref"></has-error>
+                            </div>
+
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger pull-left" data-dismiss="modal">Close
+                            </button>
+                            <button v-show="!editMode" :disabled="form.busy" type="submit" class="btn btn-primary">
+                                Create
+                            </button>
+                            <button v-show="editMode" :disabled="form.busy" type="submit" class="btn btn-success">
+                                Update
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                editMode: false,
+                form: new Form({
+                    chamaa_name: '',
+                    members_count: 0,
+                    chamaa_uuid: 0,
+                }),
+                all_chamas: []
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }, methods: {
+            /** open creating/update of chamaa information**/
+            open_my_modal() {
+                $("#create_chamaa").modal('show');
+            },
+            /**chamaa data**/
+            fetch_all_chama() {
+                this.$store.dispatch("get_chamas")
+            },
+            create_chamaa() {
+                this.$store.dispatch("save_chamas", this.form);
+            }, view_chamaa(payload) {
+
+                this.$router.push({name: 'chamaa-view',path:`chamaa-view/${payload}`, params: {'chamaa_id': payload}})
+            }
+        },
+        computed: {
+            load_all_chama() {
+                let _chamaa = this.$store.getters.ALL_CHAMAS;
+                this.all_chamas = _chamaa;
+                return _chamaa;
+            }, check_creation_status() {
+                return this.$store.getters.CHAMAA_CREATION_RESPONSE
+            }
+        },
+        created() {
+            this.fetch_all_chama();
+        },
+        watch: {
+            load_all_chama(old, new_) {
+                if (old != new_) {
+                    this.all_chamas = old;
+                }
+            },
+            // check if created
+            check_creation_status(_old, _new) {
+                if (_old == 1) {
+                    $('#create_chamaa').modal('hide');
+                } else {
+                    swal.fire(
+                        'Failed!',
+                        'There Was an Problem Please try again.',
+                        'error'
+                    )
+                }
+            }
+
+        }
+    }
+</script>
